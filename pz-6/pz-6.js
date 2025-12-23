584be00589504046038fb0c6e4644d7e8501a6a4
CircleDude <yarik.gamale.ru@gmail.com>
2025-12-23 12:59:30 +1000

    Update pz-6.js

--- pz-6/pz-6.js
+++ pz-6/pz-6.js
@@ -1,20 +1,12 @@
-const ALPH_BEGIN = 'а';
-const ALPH_END = 'ё';
-const ALPH_N = getAlphCode(ALPH_END) + 1;
+const ALPH = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя';
+const ALPH_N = ALPH.length;
 
-function printAlphabet() {
-    for (let i = ALPH_BEGIN.charCodeAt(0); i <= ALPH_END.charCodeAt(0); i++) {
-        let ch = String.fromCharCode(i);
-        document.write(`${ch} - ${i} - ${getAlphCode(ch)}<br>`);
-    }
+function getAlphCode(char) {
+    return ALPH.indexOf(char[0]);
 }
 
-function getAlphCode(ch) {
-    return ch.charCodeAt(0) - ALPH_BEGIN.charCodeAt(0);
-}
-
-function getAlphChar(c) {
-    return String.fromCharCode(+c + ALPH_BEGIN.charCodeAt(0));
+function getAlphChar(code) {
+    return ALPH[code];
 }
 
 function script(orig, key) {
@@ -31,22 +23,10 @@ function script(orig, key) {
 function transcript(text, key) {
     text = text.split('');
     key = key.split('');
-
-    // text.forEach( (el, i) => text[i] = getAlphCode(el));
-    // key.forEach( (el, i) => key[i] = getAlphCode(el));
-
-    // document.write(`text: ${text}<br>key: ${key}<br>`);
     
     text.forEach( (el, i) => {
         text[i] = getAlphChar( (getAlphCode(el) - getAlphCode(key[i % key.length]) + ALPH_N) % ALPH_N );
-        // a = el - key[i % key.length];
-        // a = (a < 0) ? a + ALPH_N : a;
-        // text[i] = a % ALPH_N;
     });
-    
-    // document.write(`cod: ${text}<br>`);
-
-    // document.write(``);
 
     return text.join('');
 }
@@ -54,15 +34,8 @@ function transcript(text, key) {
 let cipher = 'ЪЛЖСАЬДФЩЬЫЙЯОЦРТОАЕЬЕЪРЬЭЫМЭЪЕЯЧОЧВЪТЬЪБРЮДЕТЖЭАЦПОЪИАЕ'.toLowerCase();
 let keySet = ['геология', 'север', 'ставка', 'победа', 'публикация', 'планета'];
 
-// document.write(script("абвэюя","я"));
-// document.write("<br>");
-// document.write(transcript("яѐёъыь","я"));
-
 document.write(`Шифр: ${cipher}<br><br>`);
 
 keySet.forEach( (key) => {
     document.write(`Ключ: ${key}<br>Расшифровка: ${transcript(cipher,key)}<br><br>`);
 });
-
-// document.write("<br><br>");
-// printAlphabet();
